cece0bf26ec981f57bb943c8412da8444d7bbc90
andy-zy <andy.zyabrev@gmail.com>
2018-05-19 19:19:35 +0300

    chore: display-if plugin

--- src/components/SubHeading.js
+++
@@ -1,7 +0,0 @@
-// @flow
-import React from 'react'
-import { Text } from 'react-native'
-
-import { layout } from '../styles'
-
-export default (props: Object) => <Text {...props} style={[layout.subHeading, props.style]}>{props.children}</Text>
